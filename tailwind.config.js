1e2b7da6c26b2e1a11735925067db987cbdad21b
JoaoPedroSS23 <jp200209@gmail.com>
2025-05-19 20:33:55 -0300

    fix: adicionando sitemap no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', 'google5136b1145ff3f9b0.html'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', 'google5136b1145ff3f9b0.html', 'sitemap.xml'],
   theme: {
     extend: {},
   },
